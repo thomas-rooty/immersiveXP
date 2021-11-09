f639002058716cd96f68906d05e61a1f9448fe8f
thomas-rooty <essainono1@gmail.com>
2021-11-09 17:17:58 +0100

    added earth, clouds (hidden) and post processing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,106 @@
-import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
-import { PerspectiveCamera, Environment, PointerLockControls, OrbitControls, Html, useProgress } from "@react-three/drei";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Suspense } from "react";
+import ReactDOM from 'react-dom'
+import React, { useRef, Suspense } from 'react'
+import { Canvas, useLoader, extend, useFrame, useThree } from "@react-three/fiber"; //Fiber React component
+import { Html, useProgress, Stars } from "@react-three/drei"; //Drei component 
+import { EffectComposer, Bloom, Noise, Vignette, SMAA } from '@react-three/postprocessing' //Post-processing effects 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { TextureLoader } from "three/src/loaders/TextureLoader";
 
-function Loader() {
-  const { active, progress, errors, item, loaded, total } = useProgress();
-  return <Html center>{progress} % loaded</Html>;
+import "./App.css";
+
+//Edit the controls to make the Earth rotate
+extend({ OrbitControls })
+function Controls() {
+  const controls = useRef()
+  const { camera, gl } = useThree()
+  useFrame(() => controls.current.update())
+  return <orbitControls ref={controls} args={[camera, gl.domElement]} enableDamping dampingFactor={0.1} rotateSpeed={0.18} autoRotate={true} autoRotateSpeed={0.1} enableZoom={false} />
 }
 
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, "./scene.gltf");
-  return <primitive object={gltf.scene} scale={0.1} />;
-};
+//Make a loader
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress} % loaded</Html>
+}
 
+//Get textures from their types
+const name = (type) => `earth_${type}.jpg`;
+
+//Initialize the Earth
+function Earth() {
+  const [
+    colorMap,
+    displacementMap,
+    normalMap
+  ] = useLoader(TextureLoader, [
+    name("Color"),
+    name("Displacement"),
+    name("Normal")
+  ]);
+  return (
+    <>
+      <ambientLight intensity={0.2} />
+      <directionalLight />
+      <mesh>
+        {/* Width and height segments for displacementMap */}
+        <sphereBufferGeometry args={[1.8, 100, 100]} />
+        <meshStandardMaterial
+          displacementScale={0.03}
+          map={colorMap}
+          displacementMap={displacementMap}
+          normalMap={normalMap}
+        />
+      </mesh>
+    </>
+  );
+}
+
+//Initialize the Earth's clouds
+function Clouds() {
+  const [
+    colorMap
+  ] = useLoader(TextureLoader, [
+    name("Clouds")
+  ]);
+  return (
+    <>
+      <ambientLight intensity={0.2} />
+      <directionalLight />
+      <mesh>
+        {/* Width and height segments for displacementMap */}
+        <sphereBufferGeometry args={[2.6, 100, 100]} />
+        <meshStandardMaterial
+          map={colorMap}
+        />
+      </mesh>
+    </>
+  );
+}
+
+//Initialize the app
 export default function App() {
-  /* 
-  Initiallement dans le return, on l'enlève car on veut que tout fonctionne selon le PointerLockControls.
-  <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} /> 
-  */
   return (
     <div className="App">
       <Canvas>
-        <PointerLockControls />
+
+        {/*Loading screen*/}
         <Suspense fallback={<Loader />}>
-          <mesh onClick={() => console.log("clicked")}>
-            <Model />
-          </mesh>
-          <Environment preset="dawn" background />
+          <Earth />
+          <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade />
+
+          {/*Post-processing effects*/}
+          <EffectComposer multisampling={0}>
+            <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={900} />
+            <Noise opacity={0.05} />
+            <Vignette eskil={false} offset={0.1} darkness={1.3} />
+            <SMAA />
+          </EffectComposer>
+
         </Suspense>
+
+        {/*Controls the scene*/}
+        <Controls />
+
       </Canvas>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// eslint-disable-next-line no-unused-vars
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
