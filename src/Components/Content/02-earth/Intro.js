9f78bd98c21e30e2ac5c3e0d821dbf969c673b64
thomas-rooty <essainono1@gmail.com>
2021-12-20 23:37:36 +0100

    added conditional styling for project-desc-text

--- src/Components/Content/02-earth/Intro.js
+++ src/Components/Content/02-earth/Intro.js
@@ -3,7 +3,7 @@ import "./Intro.css";
 
 const Intro = (props) => {
     // Add event listeners to the li elements to toggle the class "active-project"
-    const setActiveProject = (e) => {
+    const setActiveAsset = (e) => {
         const activeProject = document.getElementsByClassName("active-project")[0];
         const project = e.target;
         if (activeProject) {
@@ -12,21 +12,28 @@ const Intro = (props) => {
         project.classList.add("active-project");
     }
 
+    // Projects texts
+    let earth_project = () => {
+        return (
+            <p className='project-description-text'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+        )
+    }
+
     return (
         <div className="intro">
             <div className='header-fadeOut'></div>
             <ul className='project-overview'>
-                <li onClick={setActiveProject} className='active-project' >Project 1</li>
-                <li onClick={setActiveProject} >Project 2</li>
-                <li onClick={setActiveProject} >Project 3</li>
-                <li onClick={setActiveProject} >Project 4</li>
+                <li onClick={setActiveAsset} className='active-project' >MOON</li>
+                <li onClick={setActiveAsset} >INTERNATIONAL SPACE STATION</li>
             </ul>
 
             <div className='project-description'>
                 <h1 className='project-description-title'>{props.value.planet}</h1>
-                <p className='project-description-text'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                </p>
+                {props.value.planet === "earth" &&
+                    earth_project()
+                }
                 <hr className='hr-project' />
             </div>
         </div>
