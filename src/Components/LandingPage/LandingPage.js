f1d72da90247a1e978c18f6849bd8005ae3fa5e1
thomas-rooty <essainono1@gmail.com>
2022-03-29 15:20:27 +0200

    removed sound + sound icon

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -15,9 +15,6 @@ const LandingPage = () => {
         navbar.classList.add("shown");
         content.classList.add("shown");
 
-        // Play the audios
-        initClickSound();
-        toggleAudio();
         landingPageButton.remove();
 
         setTimeout(() => {
@@ -25,57 +22,10 @@ const LandingPage = () => {
         }, 4000);
     }
 
-    //Init click sound effect
-    const initClickSound = () => {
-        // Distinguish between click and drag
-        let moved;
-        let downListener = (e) => {
-            moved = false;
-        }
-        let moveListener = (e) => {
-            moved = true;
-        }
-        let upListener = (e) => {
-            if (!moved) {
-                playSound('/sound/click.mp3', 0.8);
-            }
-        }
-        document.addEventListener('mousedown', downListener);
-        document.addEventListener('mousemove', moveListener);
-        document.addEventListener('mouseup', upListener);
-    }
-
-    // Play sound when the button is clicked
-    const playSound = (mp3, volume) => {
-        let audio = new Audio(mp3);
-        // Audio settings (lowering volume)
-        audio.volume = volume;
-        audio.play();
-    }
-
-    // Play bg.mp3 and manage a toggle button for play and pause
-    let [audioStatus, setAudioStatus] = useState('paused');
-    const toggleAudio = () => {
-        let audioController = document.getElementsByTagName("audio")[0];
-        if (audioStatus === 'paused') {
-            audioStatus = 'playing';
-            audioController.play();
-            audioController.volume = 0.2;
-        } else {
-            audioStatus = 'paused';
-            audioController.pause();
-        }
-        setAudioStatus(audioStatus);
-    }
-
     return (
         <Html>
             <div className="landingPage">
                 <button onClick={hideLandingPage} className="landingPage_button">&nbsp;ENTER</button>
-            </div>
-            <div className="sound-player-container">
-                <img onClick={toggleAudio} src={`/icons/bg-${audioStatus}.gif`} alt='sound-icon' className="sound-icon"/>
-                <audio loop src={'/sound/bg.mp3'}/>
             </div>
         </Html>
     );
